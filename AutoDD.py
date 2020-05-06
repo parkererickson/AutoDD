8aca68d0a02209f955fe319b57dc1c9167835f20
Fufu Fang <fangfufu2003@gmail.com>
2020-05-06 20:10:46 +0100

    fully working

--- AutoDD.py
+++ AutoDD.py
@@ -1,10 +1,13 @@
+"""
+
+"""
 from psaw import PushshiftAPI
 from datetime import datetime, timedelta
 import re
 
 
 def get_submission(n):
-    '''Returns a generator for the submission in past n days'''
+    """Returns a generator for the submission in past n days"""
     api = PushshiftAPI()
     s_date = datetime.today() - timedelta(days=n)
     s_timestamp = int(s_date.timestamp())
@@ -13,40 +16,92 @@ def get_submission(n):
                                  filter=['title', 'selftext'])
     return gen
 
-# Get yesterday's submissions
-gen = get_submission(1)
-# Regex pattern
-pattern = "[A-Z]{3,4}"
-# Dictionary containing the summaries
-title_dict = {}
-selftext_dict = {}
-all_dict = {}
 
-for i in gen:
-    if hasattr(i, 'title'):
-        title = ' ' + i.title + ' '
-        title_extracted = re.findall(pattern, title)
-        for j in title_extracted:
-            if j in title_dict:
-                title_dict[j] += 1
-            else:
-                title_dict[j] = 1
+def get_freq_list(n):
+    """
+    Return the frequency list for the past n days
 
-            if j in all_dict:
-                all_dict[j] += 1
-            else:
-                all_dict[j] = 1
+    :param int n: The number of days in the past
+    :returns:
+        - all_tbl - frequency table for all stock mentions
+        - title_tbl - frequency table for stock mentions in titles
+        - selftext_tbl - frequency table for all stock metninos in selftext
+    """
 
-    if hasattr(i, 'selftext'):
-        selftext = ' ' + i.selftext + ' '
-        selftext_extracted = re.findall(pattern, selftext)
-        for j in selftext_extracted:
-            if j in selftext_dict:
-                selftext_dict[j] += 1
-            else:
-                selftext_dict[j] = 1
+    # Get yesterday's submissions
+    gen = get_submission(n)
+    # Python regex pattern for stocks codes
+    pattern = "[A-Z]{3,4}"
+    # Dictionary containing the summaries
+    title_dict = {}
+    selftext_dict = {}
+    all_dict = {}
 
-            if j in all_dict:
-                all_dict[j] += 1
-            else:
-                all_dict[j] = 1
+    for i in gen:
+        if hasattr(i, 'title'):
+            title = ' ' + i.title + ' '
+            title_extracted = re.findall(pattern, title)
+            for j in title_extracted:
+                if j in title_dict:
+                    title_dict[j] += 1
+                else:
+                    title_dict[j] = 1
+
+                if j in all_dict:
+                    all_dict[j] += 1
+                else:
+                    all_dict[j] = 1
+
+        if hasattr(i, 'selftext'):
+            selftext = ' ' + i.selftext + ' '
+            selftext_extracted = re.findall(pattern, selftext)
+            for j in selftext_extracted:
+                if j in selftext_dict:
+                    selftext_dict[j] += 1
+                else:
+                    selftext_dict[j] = 1
+
+                if j in all_dict:
+                    all_dict[j] += 1
+                else:
+                    all_dict[j] = 1
+
+    title_tbl = sorted(title_dict.items(), key=lambda x: x[1], reverse=True)
+    selftext_tbl = sorted(selftext_dict.items(), key=lambda x: x[1],
+                          reverse=True)
+    all_tbl = sorted(all_dict.items(), key=lambda x: x[1], reverse=True)
+
+    return all_tbl, title_tbl, selftext_tbl
+
+
+def filter_tbl(tbl, min):
+    """
+    Filter a frequency table
+
+    :param list tbl: the table to be filtered
+    :param int min: the number of days in the past
+    :returns: the filtered table
+    """
+    BANNED_WORDS = [
+        'THE', 'FUCK', 'ING', 'CEO', 'USD', 'WSB', 'FDA', 'NEWS', 'FOR', 'YOU',
+        'BUY', 'HEAR', 'HIGH', 'ADS', 'FOMO', 'THIS', 'OTC', 'ELI', 'IMO',
+        'CBS', 'SEC', 'NOW', 'OVER'
+    ]
+    tbl = [row for row in tbl if row[1] > min]
+    tbl = [row for row in tbl if row[0] not in BANNED_WORDS]
+    return tbl
+
+
+def print_tbl(tbl):
+    print("Code\tFrequency")
+    for row in tbl:
+        padding = ""
+        if len(row[0]) < 4:
+            padding = ' '
+        print(str(row[0]) + padding + "\t" + str(row[1]))
+
+
+if __name__ == '__main__':
+    all_tbl, _, _ = get_freq_list(1)
+    all_tbl = filter_tbl(all_tbl, 2)
+    print_tbl(all_tbl)

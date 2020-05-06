dab803a4fcbb20027138c77f20c30d82a6b8024a
Fufu Fang <fangfufu2003@gmail.com>
2020-05-07 00:25:43 +0100

    banned 'MOON'

--- AutoDD.py
+++ AutoDD.py
@@ -103,7 +103,7 @@ def filter_tbl(tbl, min):
     BANNED_WORDS = [
         'THE', 'FUCK', 'ING', 'CEO', 'USD', 'WSB', 'FDA', 'NEWS', 'FOR', 'YOU',
         'BUY', 'HEAR', 'HIGH', 'ADS', 'FOMO', 'THIS', 'OTC', 'ELI', 'IMO',
-        'CBS', 'SEC', 'NOW', 'OVER', 'ROPE'
+        'CBS', 'SEC', 'NOW', 'OVER', 'ROPE', 'MOON'
     ]
     tbl = [row for row in tbl if row[1] > min]
     tbl = [row for row in tbl if row[0] not in BANNED_WORDS]
